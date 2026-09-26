e00ea4284b8233b71ebaa815aca14f0399f40eca
DIGI Byte <6645396+digimbyte@users.noreply.github.com>
2026-09-26 18:10:39 +1000

    new images and cleaner scene behavior

--- GUI/Scripts/Editor/UIBlockActivatorEditor.cs
+++ GUI/Scripts/Editor/UIBlockActivatorEditor.cs
@@ -8,10 +8,23 @@ namespace Aura.Editor
     internal class UIBlockActivatorEditor : UnityEditor.Editor
     {
         [InitializeOnLoadMethod]
+        private static void InitializeSceneIconVisibility()
+        {
+            EditorApplication.delayCall += HideSceneIcon;
+            EditorApplication.hierarchyChanged += HideSceneIcon;
+        }
+
         private static void HideSceneIcon()
         {
-            EditorApplication.delayCall += () =>
-                GizmoUtility.SetIconEnabled(typeof(UIBlockActivator), false);
+            foreach (GizmoInfo info in GizmoUtility.GetGizmoInfo())
+            {
+                if (info.iconEnabled && info.script is MonoScript script &&
+                    script.GetClass() == typeof(UIBlockActivator))
+                {
+                    info.iconEnabled = false;
+                    GizmoUtility.ApplyGizmoInfo(info, false);
+                }
+            }
         }
 
         public override void OnInspectorGUI() { hideFlags = target.hideFlags | HideFlags.HideInInspector; }
